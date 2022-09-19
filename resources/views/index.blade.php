32cc0fb9d9f12cc5826f1757d0e9877fb6f29e67
Andreas Bima <andreas.bima10@gmail.com>
2022-09-19 17:53:02 +0700

    Add existing project files prior to the push to GitHub.

---
+++ resources/views/index.blade.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <title>Index</title>
+    <link rel="stylesheet" href="/css/app.css">
+</head>
+
+<body>
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col">
+                <img src="/img/foto cv.jpg"
+                width="100%">
+            </div>
+            <div class="col">
+                <h1 class="text-light" style="background-color: #436191"><b>Andreas Bima</b></h1>
+                <h4 class="text-muted">Curriculum Vitae</h4><br>
+
+                <p>Saya adalah mahasiswa Teknik Informatika di STMIK LIKMI. Sekarang saya menempuh semester ke lima
+                    perkuliahan.</p><br>
+
+                <div class="row">
+                    <div class="col">
+                        <h4><b>Informasi Kontak</b></h4>
+                        <p>Phone  : 0822 9522 8586<br>
+                           E-mail : andreas.bima10@gmail.com</p>
+                        <h4><b>Keahlian</b></h4>
+                        <p class="text-justify">Saya cukup mahir dalam berbahasa inggris, saya juga suka untuk membaca, menonton, ataupun mendengarkan
+                            hal berbahasa inggris yang ada di <i>social media</i>, itu membantu saya dalam menambah kosa kata dan
+                            juga membuat saya semakin terbiasa dalam hal <i>listening</i>.
+                        </p>
+                        <h4><b>Riwayat Pendidikan</b></h4>
+                        <p><b>STMIK LIKMI</b><br>2020 - 2024</p>
+                        <p><b>SMA TRINITAS</b><br>2017 - 2020</p>
+                    </div>
+                    <div class="col">
+                        <h4><b>Pengalaman Kerja</b></h4>
+                        <p><b>ULTIMATE ORGANIZER</b><br>2022</p>
+                        <p class="text-justify"><b>HRD</b><br>Dalam perusahaan ini, saya memegang tanggung jawab sebagai HRD. Pekerjaan yang sekarang sedang saya
+                        lakukan sekarang adalah menyiapkan segala keperluan untuk <i>open recruitment</i>. Selain menjadi HRD saya juga
+                        menjadi <i>junior busniess consultant</i> dimana saya menganalisis hal-hal yang dirasa kurang tepat di perusahaan klien
+                        dan melakukan riset untuk memperbaiki hal-hal yang kurang tersebut. Saya juga pernah menulis beberapa konten sosial media
+                        untuk promosi perusahaan.</p>
+                    </div>
+                </div>
+            </div>
+        </div><br>
+        <div class="row">
+            <div class="col" style="background-color: #436191">
+                <p class="text-light" style="text-align:center"><br>© <?php echo date("Y"); ?></p>
+            </div>
+        </div>
+    </div>
+
+
+    <script src="/js/app.js"></script>
+</body>
+
+</html>
---
+++ resources/views/portfolio.blade.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <title>Portfolio</title>
+    <link rel="stylesheet" href="/css/app.css">
+</head>
+
+<body>
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col">
+                <h1 class="text-light" style="background-color: #436191"><b>Portofolio Andreas Bima</b></h1>
+                <h5>Hal-hal yang pernah dibuat</h5>
+            </div>
+        </div><br>
+        <div class="row">
+            <div class="col">
+                <img src="/img/cover-1.png" width="100%">
+                <small>Konten ini bercerita tentang penyebab kebangkrutan dari 2 brand tersebut. Tujuannya adalah
+                    menanamkan awareness kepada audience untuk menghindari hal-hal tersebut kalau tidak ingin
+                    bisnis mereka bangkrut.
+                </small>
+            </div>
+            <div class="col">
+                <img src="/img/cover-2.png" width="100%">
+                <small>Konten ini (draft) mengangkat sudut pandang iPhone yang menjadi tidak laku di pasaran bila
+                    mereka hanya memasang harga di 2 juta rupiah. Hal ini bisa terjadi karena engan harga 2 juta, iPhone
+                    tidak memiliki gengsi yang tinggi lagi dan tidak memiliki differensiasi dengan merk lain.
+                </small>
+            </div>
+            <div class="col">
+                <img src="/img/cover-3.png" width="100%">
+                <small>Konten ini (draft) membawa sudut pandang apabila kita memiliki kekayaan sebanyak Grace Tahir
+                    secara tiba-tiba. Konten ini menekankan untuk selalu memikirkan budgeting dan rencana yang matang
+                    apabila kita mendapat uang kaget supaya nanti nya uang tersebut bisa teralokasi dengan jelas.
+                </small>
+            </div>
+        </div><br>
+        <div class="row">
+            <div class="col" style="background-color: #436191">
+                <p class="text-light" style="text-align:center"><br>© <?php echo date("Y"); ?></p>
+            </div>
+        </div>
+    </div>
+
+
+    <script src="/js/app.js"></script>
+</body>
+
+</html>
